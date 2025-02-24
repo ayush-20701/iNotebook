4f8f39a4b9eeae131b8b7d19755d8d852568746d
Ayush Kumar <ayushkumar20701@gmail.com>
2025-02-25 00:32:46 +0530

    Add express-validator and lodash dependencies
    - Modified Backend/index.js
    - Added models for Notes and User
    - Updated package-lock.json and package.json
    - Added routes for auth and notes

--- Backend/index.js
+++ Backend/index.js
@@ -2,10 +2,13 @@ const express = require('express')
 const connectToMongo = require('./db')
 const app = express()
 const port = 3000
+
+app.use(express.json()) // This is a middleware that allows us to parse JSON data from the client
+
+
 app.listen(port , () => {
     console.log(`Server is running on port ${port}`)
 })
-app.get('/', (req, res) => {    
-    res.send('Hello World!')
-})
+app.use('/api/auth', require('./routes/auth'))
+
 connectToMongo()

a7b7af394ad43f2db6c95a3960a85869a8a12a84
Ayush Kumar <ayushkumar20701@gmail.com>
2025-03-01 14:12:26 +0530

    mongoDB setup done

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const User = require('../models/User')
-const router = express.Router()
+const router = express.Router() // This is a router object that has the same methods as the app object
+const { query, validationResult } = require('express-validator');
 
 router.post('/', (req, res) => {
-    const user = User(req.body)
-    user.save()
+    const user = User(req.body) // This will create a new user object
+    user.save() // This will save the user to the database
     console.log(req.body);
     
     res.send(req.body)

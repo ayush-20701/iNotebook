d1425b11877ac406d9e68b686422f207eb65a9e4
Ayush Kumar <ayushkumar20701@gmail.com>
2025-03-02 17:22:46 +0530

    created login and createUser routes

--- backend-iNotebook/index.js
+++ backend-iNotebook/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const port = 3000
+const port = 5000
 const connectToMongo = require('./db');
+app.use(express.json())
+
 connectToMongo();
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`)
 })
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+app.use('/api/auth', require('./routes/auth'))
---
+++ backend-iNotebook/routes/auth.js
@@ -0,0 +1,90 @@
+const User = require('../models/user');
+const router = require('express').Router()
+const {body, validationResult} = require('express-validator')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const JWT_SECRET = 'ayushayushayush'
+
+router.post('/createuser', [ 
+    // Express-validator
+    body('username', 'Enter a valid username').isLength({min: 3}),
+    body('password', 'Password must be at least 5 characters').isLength({min: 5}),
+], async (req, res) => {
+    const errors = validationResult(req)
+
+    // If there are errors, return Bad request and the errors
+    if (!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()})
+    }
+
+    try {
+        // Check whether the username already exists
+        if(await User.findOne({username: req.body.username})) {
+            return res.status(400).json({error: 'Username already exists'})
+        }
+
+        // Hash the password
+        const secPass = await bcrypt.hash(req.body.password, await bcrypt.genSalt(10))
+
+        
+        // Create a new user
+        await User.create({
+            username: req.body.username,
+            password: secPass
+        }).then(user =>
+            res.json(user)
+        ).catch(err => {
+            console.log(err.errmsg)
+            res.status(500).json({error: 'Internal server error'})
+        })
+        
+        // Create an authorization token using jsonwebtoken
+        const data = {
+            user:{
+                id: User.id
+            }
+        }
+        const authToken = jwt.sign(data, JWT_SECRET)
+        console.log("token: ",authToken);
+    } 
+    catch (error) {
+        console.log(error.message);
+    }
+})
+//authenticate user for login
+router.post('/login', [
+    body('username', 'Enter a valid username').isLength({min: 3}),
+    body('password', 'Password cannot be blank').exists()
+], async (req, res) => {
+    const errors = validationResult(req)
+
+    // If there are errors, return Bad request and the errors
+    if (!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()})
+    }
+
+    const {username, password} = req.body
+    try {
+        let user = await User.findOne({username})
+        if(!user) {
+            return res.status(400).json({error: "Invalid credentials"})
+        }
+        const passComp = await bcrypt.compare(password, user.password)
+        if(!passComp) {
+            return res.status(400).json({error: "Invalid credentials"})
+        }
+
+        // Create an authorization token using jsonwebtoken
+        const data = {
+            user:{
+                id: User.id
+            }
+        }
+        const authToken = jwt.sign(data, JWT_SECRET)
+        console.log("token: ",authToken);
+        res.json({ authToken });
+    } catch (error) {
+        res.send({error: error.message})
+    }
+})
+module.exports = router
